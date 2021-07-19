c9aaf6b9aa2dffdb8edc89db46a588ea084490b8
Esauceda <saucedaedu15@gmail.com>
2021-07-18 18:53:42 -0600

    Commit de Diseño Correcto

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p2_proyecto2/PerderActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_p2_proyecto2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PerderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perder)
+    }
+}
